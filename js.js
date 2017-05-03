3fc63666871a6ae2c92ca7092aff6cab65b38d94
MorganeFleury <morgane3fleury@gmail.com>
2017-05-03 17:02:45 +0200

    sauvegarde

--- js.js
+++ js.js
@@ -1,3 +1,16 @@
+/*declaration des variables*/
+
+
+var tableauMot = ["lavande", "corail", "sauge", "caramel", "cacao", "or", "canard", "ballet", "lilas", "rubis"];
+var motAleatoire = tableauMot[Math.floor(Math.random()*tableauMot.length)];
+var btnRjz = document.querySelector(".btn-rejouez");
+
+
+
+
+/*faire une boucle et generer des inputs*/
+
+
 var clavier = document.querySelector("#clavier");
 
 for( char of "abcdefghijklmnopqrstuvwxyz" ){
